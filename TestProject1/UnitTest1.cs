b3e0175fbf97d231082f9208e186d432cf89b996
patelaftab <10patelaftab@gmail.com>
2023-01-14 11:35:42 +0530

    [Aftab] Checking Email Samples in Test Case

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -85,6 +85,32 @@ namespace MsTestOnUserRegistration
             bool output = UserRegistration.SpecialCharacter1(userInput);      //UC8 min 8 char , exactly 1 special char 
             Assert.AreEqual(output, true);
         }
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]    //pass
+        [DataRow("abc-100@yahoo.com")]   //pass ..
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        [DataRow("abc", "abc@.com.my")]
+        [DataRow("abc123@gmail.a")]
+        [DataRow("abc123@.com")]
+        [DataRow("abc123@.com.com")]
+        [DataRow(".abc@abc.com")]
+        [DataRow("abc()*@gmail.com")]
+        [DataRow("abc@%*.com")]
+        [DataRow("abc..2002@gmail.com")]
+        [DataRow("abc.@gmail.com")]
+        [DataRow("abc@abc@gmail.com")]
+        [DataRow("abc@gmail.com.1a")]
+        [DataRow("abc@gmail.com.aa.au")]
+        public void EmailSample(string userInput)
+        {
+            bool output = UserDetails.EmailSample(userInput);      //UC9 Email Samples .
+            Assert.AreEqual(output, true);
+        }
     }
-}
 }
